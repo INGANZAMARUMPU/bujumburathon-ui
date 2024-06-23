b62c302536d678a71d20cf33eee1154cc90f67f9
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-24 01:37:22 +0200

    actif mcro vm

---
+++ src/components/MicroVmListe.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        
+        <div class="display_header">
+            <h1>Micro Vm Liste</h1>
+            <button @click="createMc">
+                <b-icon-plus style="size: 24px;" />
+                Creer un Micro VM</button>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>Ip Adress</th>
+                    <th>Date de Creation</th>
+                    <th>Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="item in items" :key="item.id">
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.nom }}</td>
+                    <td>{{ item.ip }}</td>
+                    <td>
+                        {{  item.date_creation}}
+                    </td>
+                    <td>
+                        {{ item.status == 1? "Online" : "Offline" }}
+                    </td>
+                    <td>
+                        <button class="btn btn-primary" @click="startPc(item.id)">Start</button>
+                      
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:["items"],
+        data(){
+            return {
+                loader:false
+            }
+        },
+        methods:{
+            startPc(pcId){
+                alert(pcId);
+                //axios.get()
+            }, 
+            createMc(){
+                alert("ok");
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+.display_header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,18 @@
           <Computer v-for="computer in computers" :item="computer"/>
         </div>
       </div>
+      <div class="main-screen">
+        <div class="computer_detail">
+          <h1>Bonjour</h1>
+        </div>
+        <div class="microvms">
+          <micro-vm-liste :items="microvms"/>
+        </div>
+
+      </div>
+      
     </div>
+    
     <DialogButton
       :item="current_button"
       @close="closeDialogs"
@@ -25,15 +36,18 @@
 import TopBar from '@/components/topbar.vue'
 import Computer from '@/components/computer.vue'
 import DialogButton from '@/components/dialog_button.vue'
+import MicroVmListe from '@/components/MicroVmListe.vue'
 
 export default {
   components: {
-    TopBar, Computer, DialogButton
+    TopBar, Computer, DialogButton,
+    MicroVmListe
   },
   data(){
     return {
       selected:{},
       computers:[],
+      microvms:[],
       buttons:this.$store.state.buttons,
       msg:"",
       button_shown: false,
@@ -61,11 +75,30 @@ export default {
       .catch(err => {
         console.error(err); 
       })
+      axios.get(this.url + "/micro-vms/", this.headers)
+      .then(res => {
+        this.microvms = res.data
+      })
+      .catch(err => {
+        console.error(err); 
+      })
+
+
     }
   }
 }
 </script>
 <style scoped>
+
+.main-screen{
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  gap: 10px;
+  width: 100%;
+  height: 100%;
+  padding: 5px 20px;
+}
 .body{
   position: relative;
 }
