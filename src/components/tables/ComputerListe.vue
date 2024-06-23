846e0072da4f7a2ecd8a40a07551f42c8c331550
Jean-Lionel <nijeanlionel@gmailcom>
2024-06-23 23:27:42 +0200

    create list machines

---
+++ src/components/tables/ComputerListe.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <table>
+            <thead></thead>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+       data(){
+        return{
+
+        }
+       }, 
+       mounted(){
+        this.get();
+       },
+       methods:{
+        get(url){
+            axios.get()
+        }
+       }, 
+       computed:{
+        currentUrl(){
+            return this.$store.state.url + '';
+        }
+       }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
